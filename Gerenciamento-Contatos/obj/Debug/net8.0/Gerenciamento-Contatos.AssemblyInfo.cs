84276db10696994a8e28d148e25a2a1b15bb2543
V1n1cyus <121128071+V1n1cyus@users.noreply.github.com>
2024-11-05 16:17:13 -0300

    Modificação De Licença

--- Gerenciamento-Contatos/obj/Debug/net8.0/Gerenciamento-Contatos.AssemblyInfo.cs
+++ Gerenciamento-Contatos/obj/Debug/net8.0/Gerenciamento-Contatos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gerenciamento-Contatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420c2b1d7ff449d69b862fc8a21aeba053408340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d53663e7fbe4a3296a1c5071f3ebd4f139524b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gerenciamento-Contatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gerenciamento-Contatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
